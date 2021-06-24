c8c5cd3050d3cda7a31a29362a83057d6fb0e7f5
tonnon <lucastonnon@gmail.com>
2021-06-23 23:03:58 -0300

    Aula 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { useHistory } from 'react-router';
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
+
+import { database } from '../services/firebase';
 
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
@@ -12,52 +15,57 @@ import '../styles/auth.scss';
 export function Home() {
     const history = useHistory();
     const { user, singInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
-        if(!user) {
+        if (!user) {
             await singInWithGoogle();
         }
+        
         history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('A sala não existe');
+            return;
+        }
+
+        history.push(`rooms/${roomCode}`);
     }
     
     return (
         <div id="page-auth">
             <aside>
-                <img 
-                    src={illustrationImg}
-                    alt="Ilustração simbolizando perguntas e respostas" 
-                />
-                    <strong>
-                        Crie salas de Q&amp;A ao-vivo
-                    </strong>
-                    <p>
-                        Tire dúvidas da sua audiência em tempo-real
-                    </p>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong> Crie salas de Q&amp;A ao-vivo </strong>
+                <p> Tire dúvidas da sua audiência em tempo-real </p>
             </aside>
             <main>
                 <div className="main-content">
-                    <img 
-                        src={logoImg}
-                        alt="Letmeask" 
-                    />
+                    <img src={logoImg} alt="Letmeask" />
                     <button onClick={handleCreateRoom} className="create-room">
-                        <img 
-                            src={googleIconImg} 
-                            alt="Logo do Google" 
-                        />
+                        <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">
                         ou entre em uma sala
                     </div>
-                    <form >
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
                         />
-                        <Button type="submit">
-                            Entrar na sala
-                        </Button>
+                        <Button type="submit"> Entrar na sala </Button>
                     </form>
                 </div>
             </main>
